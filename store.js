59a2c1ce406bd385b94bada45e9d52de87f8a297
Moataz <Moataz@local.com>
2021-08-26 08:36:48 +0200

    Finalized login logic

--- store.js
+++ store.js
@@ -1,15 +1,31 @@
+const Users = require('./users')
 const express = require('express')
+const bcrypt = require('bcrypt')
+const cors = require('cors')
+const users = require('./users')
 const app = express()
 
-const port = 3000
+const port = 8080
+
+app.use(cors());
 
 app.get('/', (req,res) => {
     res.send('Hello Worldo')
 })
 
-app.get('/login', (req,res) => {
-    console.log("Login recieved");
+app.post('/login', (req,res) => {
+    const userName = req.headers.authorization.split(';')[0]
+    const password = req.headers.authorization.split(';')[1]
+    Users.findIndex(row => row.userName == userName && row.password == password) != -1 ? res.send("LoggedIn") : res.send("User Not Found") 
 })
+
+
+app.post('/signUp', (req,res) => {
+    const email = req.headers.authorization.split(';')[0]
+    const userName = req.headers.authorization.split(';')[1]
+    const password = req.headers.authorization.split(';')[2]
+})
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
---
+++ users.js
@@ -0,0 +1,6 @@
+const users = [
+    {userName:'ashraf', password:'123@123qw'},
+    {userName:'Mohamed', password:'#@!@123qw'}
+]
+
+module.exports = users
