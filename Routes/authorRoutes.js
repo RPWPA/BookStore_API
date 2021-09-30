397e2542459fec953a3bf112f629e166a9e4459e
Moataz <Moataz@local.com>
2021-09-30 08:16:35 +0200

    getAuthorById is now working

--- Routes/authorRoutes.js
+++ Routes/authorRoutes.js
@@ -13,24 +13,39 @@ app.get('/getAllAuthors', (req,res) => {
     })
 })
 
+app.post('/getAuthorById', async (req,res) => {
+    console.log(req.body.authorId)
+    await authors.findById(req.body.authorId).exec()
+    .then(result => {
+        if(!result)
+            res.sendStatus(404)
+        else{
+            res.status(200).send(result)
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(404).send(err)
+    })
+})
+
 app.post('/addAuthor', (req,res) => {
     console.log(req.body)
     const author = new authors({...req.body})
-    author.save().then(res => {
-        res.status(200).send(res)
+    author.save().then(result => {
+        res.status(200).send(result)
     })
     .catch(err => {
-        res.status(406).send(err)
+        res.status(400).send(err)
     })
 })
 
 app.post('/updateAuthor', (req,res) => {
-    let author = authors.findOne(req.body.name)
-    author.save().then(res => {
-        res.status(200).send(res)
+    authors.updateOne({where: {_id : req.body.authorId}}, {name: req.body.name, birthday: req.body.birthday}).then(result => {
+        res.status(200).send(result)
     })
     .catch(err => {
-        res.status(406).send(err)
+        res.status(400).send(err)
     })
 })
 
